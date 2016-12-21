0c3a69305b8d225b69db969cb7637c81eb143046
Teodor Garzdin <tsg@hugelawn.com>
2016-12-21 04:21:14 +0200

    Add a list view for all my devices.

--- components/start.js
+++ components/start.js
@@ -2,35 +2,82 @@ import React, { Component } from 'react';
 import {
   AsyncStorage,
   StyleSheet,
+  Navigator,
   Text,
   View,
-  Button
+  TouchableHighlight,
+  ListView
 } from 'react-native';
+import { routes } from './app';
 
 class Start extends Component {
   constructor(props) {
     super(props)
-    this.logout = this.logout.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+    this.state = {
+      data: [],
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      }),
+      loaded: false
+    }
   }
 
-  logout() {
-    AsyncStorage.removeItem('@SmartGarden:token', (error) => {
-      if (error) {
-        console.log(error);
+  fetchData() {
+    fetch('https://smartgardenapi.herokuapp.com/boards/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Token': this.props.token
       }
     })
-    this.props.navigator.replace({ name: 'login' });
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        data: responseJson.boards,
+        dataSource: this.state.dataSource.cloneWithRows(responseJson.boards),
+        loaded: true
+      });
+    })
+    .done();
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  pressRow(row) {
+    this.props.navigator.push({ name: 'details', data: this.state.data[row] })
+  }
+
+  renderRow(rowData, section, row) {
+    return (
+      <TouchableHighlight
+        style={styles.row}
+        onPress={() => this.pressRow(row)}
+      >
+        <Text style={styles.rowText}>{rowData.name}</Text>
+      </TouchableHighlight>
+    )
   }
 
   render() {
+    if (!this.state.loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>
+            Loading devices...
+          </Text>
+        </View>
+      )
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Logout</Text>
-        <Button
-          color="#841584"
-          onPress={this.logout}
-          title="Logout"
-          accessibilityLabel="Login"
+        <ListView
+          style={styles.list}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
         />
       </View>
     )
@@ -40,9 +87,19 @@ class Start extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#192C3F'
+  },
+  list: {
+    marginTop: 65
+  },
+  row: {
+    flexDirection: 'row',
+    height: 40,
+    paddingTop: 12
+  },
+  rowText: {
+    color: '#ffffff',
+    marginLeft: 20
   }
 })
 
