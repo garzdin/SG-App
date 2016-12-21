ccfd4fc3cce66a88e035d25ba17bcd4b21775b94
Teodor Garzdin <tsg@hugelawn.com>
2016-12-21 04:20:54 +0200

    Add a navigation bar.

--- components/app.js
+++ components/app.js
@@ -4,42 +4,59 @@ import {
   StyleSheet,
   StatusBar,
   Navigator,
-  View
+  TouchableHighlight,
+  View,
+  Text
 } from 'react-native';
 import Login from './login';
 import Start from './start';
+import Details from './details';
+
+let routesList = [
+  { index: 0, component: Login, title: 'Login' },
+  { index: 1, component: Start, title: 'Devices' },
+  { index: 2, component: Details, title: 'Details' }
+];
 
 let routes = {
-  login: Login,
-  start: Start
-};
+  'login': routesList[0],
+  'devices': routesList[1],
+  'details': routesList[2]
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.renderScene = this.renderScene.bind(this);
     this.state = {
+      isLoading: true,
       isLoggedIn: false,
       token: ''
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     AsyncStorage.getItem('@SmartGarden:token', (error, result) => {
       if (result) {
         this.setState({ isLoggedIn: true, token: result });
       } else {
         this.setState({ isLoggedIn: false, token: '' });
       }
+      this.setState({ isLoading: false });
     })
   }
 
   renderScene(route, navigator) {
-    let RouteComponent = routes[route.name];
-    return <RouteComponent route={route} navigator={navigator} token={this.state.token} />;
+    let RouteComponent = routes[route.name].component;
+    return <RouteComponent route={route} navigator={navigator} token={this.state.token} data={route.data} />;
   }
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}></View>
+      )
+    }
     return (
       <View style={styles.container}>
         <StatusBar
@@ -47,8 +64,67 @@ class App extends Component {
         />
         <Navigator
           style={styles.container}
-          initialRoute={{ name: this.state.isLoggedIn ? 'start' : 'login' }}
+          initialRoute={{ name: this.state.isLoggedIn ? 'devices' : 'login' }}
           renderScene={this.renderScene}
+          navigationBar={
+            <Navigator.NavigationBar
+              routeMapper={{
+               LeftButton: (route, navigator, index, navState) => {
+                 switch (route.name) {
+                  case 'details':
+                    return (
+                      <TouchableHighlight
+                        onPress={() => {
+                          navigator.pop()
+                        }}
+                      >
+                        <Text style={styles.leftBarButton}>Back</Text>
+                      </TouchableHighlight>
+                    )
+                    break
+                  default:
+                    return null
+                 }
+               },
+               RightButton: (route, navigator, index, navState) => {
+                if (route.name === 'devices') {
+                  return (
+                    <TouchableHighlight
+                      onPress={() => {
+                        AsyncStorage.removeItem('@SmartGarden:token', (error) => {
+                          if (error) {
+                            console.log(error);
+                          }
+                          navigator.replace({ name: 'login' })
+                        })
+                      }}
+                    >
+                      <Text style={styles.rightBarButton}>Logout</Text>
+                    </TouchableHighlight>
+                  );
+                } else {
+                  return null;
+                }
+               },
+               Title: (route, navigator, index, navState) => {
+                 switch (route.name) {
+                   case 'login':
+                     return (<Text style={styles.title}>Login</Text>)
+                     break
+                  case 'devices':
+                       return (<Text style={styles.title}>Devices</Text>)
+                       break
+                  case 'details':
+                    return (<Text style={styles.title}>Details</Text>)
+                    break
+                  default:
+                    return null
+                 }
+               }
+              }}
+              style={styles.navigation}
+            />
+          }
         />
       </View>
     )
@@ -58,7 +134,28 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  navigation: {
+    backgroundColor: '#48CFAD'
+  },
+  title: {
+    color: '#ffffff',
+    fontWeight: '600',
+    paddingTop: 10
+  },
+  leftBarButton: {
+    color: '#ffffff',
+    fontWeight: '600',
+    paddingTop: 10,
+    marginLeft: 10
+  },
+  rightBarButton: {
+    color: '#ffffff',
+    fontWeight: '600',
+    paddingTop: 10,
+    marginRight: 10
   }
 })
 
 export default App;
+export { routes };
